a314698eea822fb310eb5df37f1608d0a9ecd905
LaurelFR <128193405+LaurelFR@users.noreply.github.com>
2023-08-25 13:02:15 -0600

    Added additional styling

--- src/Translate.js
+++ src/Translate.js
@@ -4,13 +4,22 @@ import "./Translate.css";
 export default function Translate() {
   return (
     <div className="Translate">
-      <h1 className="header">Translate</h1>
+      <h1 className="header">
+        Translate <i class="fa-solid fa-language"></i>
+      </h1>
       <div className="row">
-        <div className="col">
+        <div className="col-5">
           <h3>English</h3>
-          <input type="textarea" className="translate-text" />
+          <input
+            type="textarea"
+            className="translate-text"
+            placeholder="Enter text"
+          />
         </div>
-        <div className="col">
+        <div className="col-1">
+          <i class="fa-solid fa-repeat"></i>
+        </div>
+        <div className="col-5">
           <h3>Another language</h3>
           <input type="textarea" className="translate-text" />
         </div>
